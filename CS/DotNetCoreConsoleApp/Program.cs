62e9a03ab1e00882d4634015298a037f13dc200e
David Fields (ANALOG) <dfields@microsoft.com>
2019-09-06 14:22:10 -0700

    Experimental, incomplete changes to enable .NET Core 3.0

---
+++ CS/DotNetCoreConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DotNetCoreConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WinRTComponent.Class winRTClass = new WinRTComponent.Class();
+            String textFromComponent = winRTClass.MyProperty.ToString();
+            Console.WriteLine(textFromComponent);
+        }
+    }
+}
